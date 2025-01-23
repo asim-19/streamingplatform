5e4fc1c4c00fd53258591845c75e03cf400a502f
Asim Shrivastava <97615477+asim-19@users.noreply.github.com>
2025-01-23 16:36:02 +0530

    Update TitleCards.jsx

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 const TitleCards = ({title, category}) => {
 //Jaigurudev:)
- // Done
+ const done;
   const [apiData, setApiData] = useState([])
    //const cardsRef = useRef();
   const options = {
